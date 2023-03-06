ae6ee80f5df15961b717c90326c80b6baff60fbc
Pablo Dominguez <pasaxo@gmail.com>
2023-03-06 17:10:31 -0300

    req04

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -1,12 +1,7 @@
-SELECT * FROM `SpotifyClone`.`user`;
-
-SELECT * FROM `SpotifyClone`.playback_history;
-
-SELECT DISTINCT a.name_user AS 'pessoa_usuaria', 
-IF(YEAR(b.date_playback) >= 2021, 'Ativa', 'Inativa') AS 'status_pessoa_usuaria'
-
+SELECT a.name_user AS 'pessoa_usuaria',
+IF(MAX(YEAR(b.date_playback)) >= 2021, 'Ativa', 'Inativa')AS 'status_pessoa_usuaria'
 FROM `SpotifyClone`.user AS a
 LEFT JOIN `SpotifyClone`.playback_history AS b
 ON a.id_user = b.id_user
-
+GROUP BY a.name_user
 ORDER BY a.name_user;
